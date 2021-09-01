5fd4a5114b0760abfb5ab2723fb678a6c3c51493
tupurp <tupurpcheung@gmail.com>
2021-09-01 17:59:17 +0800

    节点格式的定义，分为内部节点，叶子节点

--- src/db-constant.c
+++ src/db-constant.c
@@ -48,45 +48,71 @@ const uint32_t EMAIL_SIZE = 255;
 const uint32_t ROW_SIZE = 291;
 
 
-
-
-
 /**
-*  叶子节点的头部布局
-*  节点类型：1个字节，偏移量0
-*  是否是根节点：1个字节，偏移量1个字节
-*  父节点指针：4个字节，偏移量2个字节
-*  子节点个数：4个字节，偏移量6个字节
-*
-* 	故叶子节点的头部信息共占用 10个字节
-**/
+ * 节点（node）的公共头布局
+ * 内部节点和叶子节点都有这些页（节点）头
+ */
 
-//节点类型的大小和偏移量
+//节点类型（分为内部节点和叶子节点）的大小和偏移量
 const uint32_t NODE_TYPE_OFFSET = 0;
 const uint32_t NODE_TYPE_SIZE = 1;
-
 
 //根节点的大小和偏移量
 const uint32_t IS_ROOT_OFFSET = 1;
 const uint32_t IS_ROOT_SIZE = 1;
 
-
 //父节点指针的大小和偏移量
 const uint32_t PARENT_POINTER_OFFSET = 2;
 const uint32_t PARENT_POINTER_SIZE = 4;
 
+//公共头节点的大小
 const uint8_t COMMON_NODE_HEADER_SIZE = 6;
+
+
+/**
+ * 内部节点（internal node）的头布局
+ *
+ */
+//子节点个数
+const uint32_t INTERNAL_NODE_NUM_KEYS_OFFSET = 6;
+const uint32_t INTERNAL_NODE_NUM_KEYS_SIZE = 4;
+
+//最右边节点的指针
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_OFFSET =  10;
+const uint32_t INTERNAL_NODE_RIGHT_CHILD_SIZE = 4;
+
+//内部节点头部布局的大小，公共头+内部节点特有头
+const uint32_t INTERNAL_NODE_HEADER_SIZE = 14;
+
+
+
+/**
+* 内部节点的主体布局，由cell组成的数组
+*/
+//子节点指针
+const uint32_t INTERNAL_NODE_CHILD_SIZE = 4;
+//左侧子节点中最大的key
+const uint32_t INTERNAL_NODE_KEY_SIZE = 4;
+//cell大小
+const uint32_t INTERNAL_NODE_CELL_SIZE = 8;
+
+
+/**
+*  叶子节点的特有头部布局
+*  子节点个数：4个字节，偏移量6个字节
+* 	故叶子节点的头部信息共占用 公共头节点+叶子节点特有头布局 = 10个字节
+**/
+
 
 //子节点个数的大小和偏移量
 const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = 6;
 const uint32_t LEAF_NODE_NUM_CELLS_SIZE = 4;
 
-
 //叶子节点头部布局的大小，10个字节
 const uint32_t LEAF_NODE_HEADER_SIZE = 10;
 
 /**
-* 叶子节点的主体布局
+* 叶子节点的主体布局，由key+row 组成的数组
 */
 //一个叶子节点的主键的key大小
 const uint32_t LEAF_NODE_KEY_OFFSET = 0;
@@ -106,3 +132,16 @@ const uint32_t LEAF_NODE_SPACE_FOR_CELLS = 4096 - 10;
 
 //一页最多可以存放多少个叶子节点
 const uint32_t LEAF_NODE_MAX_CELLS = 4086 / 295;
+
+
+/**
+ * 叶子节点的分裂
+ *
+ * 为了保持树的平衡，我们在两个新节点之间均匀地分配 cell。
+ * 如果一个叶子节点可以容纳 N 个 cell，那么在分裂的过程中，
+ * 我们需要在两个节点之间分配 N+1 个 cell（N 个原 cell 加一个新的 cell）。
+ * 如果 N+1 是奇数，我就选择左节点来获得一个以上的 cell。
+ * 
+ */
+const uint32_t LEAF_NODE_RIGHT_SPLIT_COUNT = (4086 / 295 + 1) / 2;
+const uint32_t LEAF_NODE_LEFT_SPLIT_COUNT = (4086 / 295 + 1) - ((4086 / 295 + 1) / 2);
--- src/db-constant.h
+++ src/db-constant.h
@@ -1,6 +1,9 @@
 #include <stdint.h>
 
-/*页大小，4096字节,即4Kb*/
+/**
+ * 页大小，4096字节,即4Kb
+ * 一个页就是B+tree的一个节点
+*/
 extern const uint32_t PAGE_SIZE;
 
 /**
@@ -46,61 +49,97 @@ extern const uint32_t EMAIL_SIZE ;
 extern const uint32_t ROW_SIZE ;
 
 
-
-
-
 /**
-*  叶子节点的头部布局
-*  节点类型：1个字节，偏移量0
-*  是否是根节点：1个字节，偏移量1个字节
-*  父节点指针：4个字节，偏移量2个字节
-*  子节点个数：4个字节，偏移量6个字节
-*
-* 	故叶子节点的头部信息共占用 10个字节
-**/
-
-//节点类型的大小和偏移量
+ * 节点（node）的公共头布局
+ * 内部节点和叶子节点都有这些页（节点）头
+ */
+//节点类型（分为内部节点和叶子节点）的大小和偏移量
 extern const uint32_t NODE_TYPE_OFFSET ;
 extern const uint32_t NODE_TYPE_SIZE ;
 
 
-//根节点的大小和偏移量
+//是否是根节点的大小和偏移量
 extern const uint32_t IS_ROOT_OFFSET ;
 extern const uint32_t IS_ROOT_SIZE ;
-
 
 //父节点指针的大小和偏移量
 extern const uint32_t PARENT_POINTER_OFFSET ;
 extern const uint32_t PARENT_POINTER_SIZE ;
 
+//节点公共头的大小
 extern const uint8_t COMMON_NODE_HEADER_SIZE ;
 
+
+/**
+ * 内部节点（internal node）的头布局
+ *
+ */
+//子节点个数
+extern const uint32_t INTERNAL_NODE_NUM_KEYS_OFFSET ;
+extern const uint32_t INTERNAL_NODE_NUM_KEYS_SIZE ;
+
+//最右边节点的指针
+extern const uint32_t INTERNAL_NODE_RIGHT_CHILD_OFFSET ;
+extern const uint32_t INTERNAL_NODE_RIGHT_CHILD_SIZE;
+
+//内部节点头部布局的大小，公共头+内部节点特有头
+extern const uint32_t INTERNAL_NODE_HEADER_SIZE ;
+
+/**
+* 内部节点的主体布局，由cell组成的数组
+*/
+//子节点指针
+extern const uint32_t INTERNAL_NODE_CHILD_SIZE;
+//左侧子节点中最大的key
+extern const uint32_t INTERNAL_NODE_KEY_SIZE;
+//cell大小
+extern const uint32_t INTERNAL_NODE_CELL_SIZE;
+
+/**
+*  叶子节点（leaf node）的头部布局
+*  子节点个数：4个字节，偏移量6个字节
+*
+**/
 //子节点个数的大小和偏移量
 extern const uint32_t LEAF_NODE_NUM_CELLS_OFFSET ;
 extern const uint32_t LEAF_NODE_NUM_CELLS_SIZE ;
 
 
-//叶子节点头部布局的大小，10个字节
+//叶子节点头部布局的大小，公共头+叶子节点特有头
 extern const uint32_t LEAF_NODE_HEADER_SIZE ;
 
 /**
-* 叶子节点的主体布局
+* 叶子节点(leaf node)的主体布局
 */
-//一个叶子节点的主键的key大小
+//一个叶子节点的主键的(key)大小
 extern const uint32_t LEAF_NODE_KEY_OFFSET;
 extern const uint32_t LEAF_NODE_KEY_SIZE;
 
 
-//一个叶子节点的数据大小
+//一个叶子节点的数据(row)大小
 extern const uint32_t LEAF_NODE_VALUE_OFFSET;
 extern const uint32_t LEAF_NODE_VALUE_SIZE ;
 
 
-//一个叶子节点的大小 = 数据+主键
+//一个节点数据 = key+row
 extern const uint32_t LEAF_NODE_CELL_SIZE;
 
-//一页（4k=4094字节）可用于存储节点数据的空间=4094字节-页头节点
+//一页（4k=4094字节）可用于存储节点数据(key+row)的空间=4094字节-头节点大小
 extern const uint32_t LEAF_NODE_SPACE_FOR_CELLS;
 
-//一页最多可以存放多少个叶子节点
+//一个叶子节点最多可以存放多少个节点数据 key+row
 extern const uint32_t LEAF_NODE_MAX_CELLS ;
+
+
+
+/**
+ * 叶子节点的分裂
+ *
+ * 为了保持树的平衡，我们在两个新节点之间均匀地分配 cell。
+ * 如果一个叶子节点可以容纳 N 个 cell，那么在分裂的过程中，
+ * 我们需要在两个节点之间分配 N+1 个 cell（N 个原 cell 加一个新的 cell）。
+ * 如果 N+1 是奇数，我就选择左节点来获得一个以上的 cell。
+ * 
+ */
+extern const uint32_t LEAF_NODE_RIGHT_SPLIT_COUNT;
+extern const uint32_t LEAF_NODE_LEFT_SPLIT_COUNT;
